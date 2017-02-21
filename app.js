29a3948ce14206a6848014719c6eb62afbc52898
Jesper Lund <jeslu246@student.liu.se>
2017-02-21 11:35:28 +0100

    server setup updated

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 /**
- * 
  * Server Setup
+ * 
  * Run in browser as "127.0.0.1:3000" 
  *
  */
-
 
 var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-   // res.end('scrackin?');
    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
    myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('listening to port 3000');
-
-//console.log('sup');
 
 //console.log(__filename);
