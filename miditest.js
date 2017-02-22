d2f46243a89cca40fabd89a01d84952e37c68b25
Jesper Lund <jeslu246@student.liu.se>
2017-02-22 12:06:33 +0100

    Playing from soundfont working. Need to fix stop()

--- miditest.js
+++ miditest.js
@@ -1,36 +1,42 @@
-//Midi Processing -----------------------------------------------------------------------------
+/**
+ * Fetching MIDI-data from MIDI Input
+ * Using soundfont-player.js for loading soundfont and play sound
+ * Using WebMidi for eventListerners
+ */
 
-//Visualization -------------------------------------------------------------------------------
-//Soundfont.instrument(new AudioContext(), 'soundfonts/acoustic_grand_piano-ogg.js').then(function (piano);
-  
+Soundfont.instrument(new AudioContext(), './soundfonts/acoustic_grand_piano-ogg.js').then(function (piano){
 
-WebMidi.enable(function(err) { 
+  WebMidi.enable(function(err) { 
  
       if (err) 
         console.log("WebMidi could not be enabled");
-
       else
         console.log("WebMidi is enabled!");
 
      var input = WebMidi.inputs[0];
 
    //  Listening for a 'note on' message (on all channels) 
-
      input.addListener('noteon', "all", function(e) 
       { 
-        Soundfont.instrument(new AudioContext(), 'acoustic_grand_piano') 
-        {
-        console.log("Note: " + e.note.name + "  Octave: " + e.note.octave);
-        console.log("Velocity: " + e.rawVelocity);
-        var key = e.note.name + e.note.octave;
-      //  piano.play(key);
-        }
+          let note = e.note.name,
+              octave = e.note.octave,
+              velocity = e.rawVelocity / 120;
 
-
-     
+          console.log("Note: " + note + " | Octave: " + octave + " | Velocity: " + velocity);
+          let key = e.note.name + e.note.octave;
+          piano.play(key, 0, { duration: 5}, {gain: 1});
+  
       });
 
+      input.addListener('noteoff', 'all', function(e){
+          piano.stop();   
+      });
+
+  });
+
 });
+
+
 
 
  
