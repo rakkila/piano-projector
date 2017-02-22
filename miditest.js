890e606bb98028017ae40f04730b6ad7bd353d97
Rasmus Ståhl <Rasmus Sthl>
2017-02-22 15:10:18 +0100

    added working gain, changed a scripts path in index

--- miditest.js
+++ miditest.js
@@ -1,10 +1,11 @@
 /**
  * Fetching MIDI-data from MIDI Input
  * Using soundfont-player.js for loading soundfont and play sound
- * Using WebMidi for eventListerners
+ * Using WebMidi for eventListeners
  */
 
-Soundfont.instrument(new AudioContext(), './soundfonts/acoustic_grand_piano-ogg.js').then(function (piano){
+var ac = new AudioContext();
+Soundfont.instrument(ac, './soundfonts/acoustic_grand_piano-ogg.js').then(function (piano){
 
   WebMidi.enable(function(err) { 
  
@@ -18,19 +19,26 @@ Soundfont.instrument(new AudioContext(), './soundfonts/acoustic_grand_piano-ogg.
    //  Listening for a 'note on' message (on all channels) 
      input.addListener('noteon', "all", function(e) 
       { 
+ 
           let note = e.note.name,
               octave = e.note.octave,
               velocity = e.rawVelocity / 120;
 
           console.log("Note: " + note + " | Octave: " + octave + " | Velocity: " + velocity);
+
           let key = e.note.name + e.note.octave;
-          piano.play(key, 0, { duration: 5}, {gain: 1});
-  
+          piano.play(key, 0, { duration: 5, gain: velocity});
+
+            input.addListener('noteoff', 'all', function(e){
+            console.log("OFF");
+            piano.play(key).stop(ac.currentTime);  
+        //    piano.stop(ac.currentTime);   
+        });
       });
 
-      input.addListener('noteoff', 'all', function(e){
-          piano.stop();   
-      });
+    //  input.addListener('noteoff', 'all', function(e){
+   //       piano.stop();   
+   //   });
 
   });
 
