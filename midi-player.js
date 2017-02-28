dc29241ff83215a75384908add28f6bfa1b0bdc3
Rasmus Ståhl <Rasmus Sthl>
2017-02-28 15:59:50 +0100

    added soundfonts, midifiles, jquery, textfile and js file to read from textfile

--- midi-player.js
+++ midi-player.js
@@ -3,9 +3,9 @@ function startMidiPlayer(){
 
     var player = MIDI.Player;
 
-    player.timeWarp = 1.5; // speed the song is played back
+  //  player.timeWarp = 1.5; // speed the song is played back
 
-    player.loadFile('MIDI-files/closer.mid', function(){
+    player.loadFile('MIDI-files/purplechanged2.mid', function(){
         console.log('Loaded MIDI file contains following instruments: ');
         console.log(player.getFileInstruments());
 
@@ -15,13 +15,17 @@ function startMidiPlayer(){
             onsuccess: function(){
 
                 //player.timeWarp = 1.5; // speed the song is played back
-
+                
+                player.start();
                 MIDI.Player.addListener(function(data) { 
-                    if(data.channel = 1)
-                        console.log('Note: ' + data.note + '  | Time: ' + data.now);
+                    var channel = data.channel;
+                  
+                    if(channel == 2){
+                        console.log('Note: ' + data.note + '  | Time: ' + data.now + '  | channel: ' + data.channel);
+                        
+                   }
+                        
                 });
-
-            player.start();
 
             }      
         })
---
+++ textreader.js
@@ -0,0 +1,9 @@
+$.get('text.txt', function(data) {
+    
+   console.log(data)
+    var fh = OpenTextFile("text.txt");
+    fh.WriteLine('123' + ',' + '456');
+   // fh.Close();
+    console.log('JAAA');
+}, 'text');
+
