9a20d8cdee10eafcc5698d9d77682d451086f9da
Jesper Lund <jeslu246@student.liu.se>
2017-03-21 15:03:56 +0100

    + note blocks connected to song notes + Created NoteBlock class

---
+++ NoteBlock.js
@@ -0,0 +1,46 @@
+/**
+ * Class: Noteblocks (visualization)
+ */
+
+class NoteBlock{
+
+    constructor(note, width, startTime, stopTime){
+
+        this.note = note;
+        this.blockWidth = width;
+        this.startTime = startTime;
+        this.noteLength = (stopTime - startTime) * 0.2;
+        this.mesh = new THREE.Mesh(new THREE.BoxGeometry(this.blockWidth,this.noteLength,0.2), 
+                    new THREE.MeshPhongMaterial({color: 0x0000ff}))
+
+    }
+
+    toString () {
+        return "Note: " + this.note + '  StartTime: ' + this.startTime + ' NoteLength: ' + this.noteLength;
+    }
+
+}
+
+function loadNoteBlocks(){
+
+    var songArray = [];
+    songArray = parseSongData(song.getSongName());
+    let size = songArray.length;
+    var noteBlockArray = new Array(size);
+    let noteWidth;
+
+    for(let i = 0; i < size; ++i){
+         
+        let note = songArray[i].note;
+
+        if(note.includes("b"))
+            noteWidth = 0.5;
+        else
+            noteWidth = 1;
+
+        noteBlockArray[i] = new NoteBlock(songArray[i].note, noteWidth, songArray[i].starttime, songArray[i].stoptime);
+    }
+
+    return noteBlockArray;
+    
+}
