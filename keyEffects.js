f38fdbe9149904ff5f9aefe211e7de77a4b1e1b9
Rebecca Akkila <rebak278@student.liu.se>
2017-03-24 16:16:08 +0100

    key effects

---
+++ keyEffects.js
@@ -0,0 +1,22 @@
+function lightUp(index)
+{ 
+     NoteBlockArray[index].mesh = THREE.MeshPhongMaterial({color: 0xffffff}))
+     //sound?
+
+}
+
+function lightOff(index)
+{ 
+     NoteBlockArray[index].mesh = THREE.MeshPhongMaterial({color: 0x000000}))
+
+}
+
+function lightWrong(index)
+{ 
+     NoteBlockArray[index].mesh = THREE.MeshPhongMaterial({color: 0xff0000}))
+     //sound??
+     if(NoteBlockArray[index].stoptime - time < 0.1){
+         lightOff(index);
+     }
+
+}
