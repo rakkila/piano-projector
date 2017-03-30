0d9745aff7881d9242eb9c3b5359b26472721a8c
Jonas Kinnvall <jonaskinnvall@gmail.com>
2017-03-30 17:05:20 +0200

    Fix tempo 3D, new Roses

--- PointGiver.js
+++ PointGiver.js
@@ -10,24 +10,24 @@ function noteon(keyOn, timeOn, pArray)
         if (keyOn == pArray[i].note)
         {
     
-            if( Math.abs(pArray[i].starttime - timeOn) < 10 && timeOn < pArray[i].stoptime)
+            if( Math.abs(pArray[i].starttime - timeOn) < 15 && timeOn < pArray[i].stoptime)
             {
-                console.log('YES!');
+                //console.log('YES!');
                 lightUp(keyOn, pArray[i].starttime);
             }
             //pArray[i].points = pArray[i].points + 50;
         }
         else
            { 
-               lightWrong(keyOn, pArray[i].starttime);
+               /*lightWrong(keyOn, pArray[i].starttime);
                //fullScoreCounter = 0;
-               //starPower = false;
+               //starPower = false;*/
            }
     }
 }
 
 //Function looking for a match with 'noteOff'
-//Deducts points if the first condition is violated. 
+//Deducts points if the first condition is// violated. 
 function noteoff(keyOff, timeOff, pArray, aPoint)
 {
 
@@ -36,19 +36,19 @@ function noteoff(keyOff, timeOff, pArray, aPoint)
         if (keyOff == pArray[i].note)
         {
 
-            console.log('FOUND KEYOFF!!! stoptime = ' + pArray[i].stoptime);
+            //console.log('FOUND KEYOFF!!! stoptime = ' + pArray[i].stoptime);
             lightOff(keyOff, pArray[i].stoptime);
             
             if((Math.abs(pArray[i].stoptime - timeOff) > 15) && pArray[i].points != 0)
             {    
                 pArray[i].points -= Math.abs(pArray[i].stoptime - timeOff) * 0.5; 
 
-                lightWrong(keyOn, starttime);
+                //lightWrong(keyOn, starttime);
                // fullScoreCounter = 0;
                 //starPower = false;
 
-                console.log('stoptime - timeOff: ' + Math.abs(pArray[i].stoptime - timeOff));
-                console.log('You lost: ' + Math.abs(pArray[i].stoptime - timeOff) * 0.5 + ' points. Index: '+ i + ' Array length: ' + pArray.length);
+                //console.log('stoptime - timeOff: ' + Math.abs(pArray[i].stoptime - timeOff));
+                //console.log('You lost: ' + Math.abs(pArray[i].stoptime - timeOff) * 0.5 + ' points. Index: '+ i + ' Array length: ' + pArray.length);
                 aPoint = pArray[i].points;
                 pArray.splice(i, 1);
                 break;
@@ -57,7 +57,7 @@ function noteoff(keyOff, timeOff, pArray, aPoint)
             else if((Math.abs(pArray[i].stoptime - timeOff) < 15) && pArray[i].points != 0)
             {
 
-                console.log('Full score !!!?!?!?!?!?!?!??!?!?!?!?!?!?!??!?!?!');
+                //console.log('Full score !!!?!?!?!?!?!?!??!?!?!?!?!?!?!??!?!?!');
                /* if(starPower == false) fullScoreCounter += 1;
                 if(fullScoreCounter == 3) 
                 {   
