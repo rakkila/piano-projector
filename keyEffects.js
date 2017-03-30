22b29ba808db327341acb067c3dbedf505b1b2af
Jesper Lund <jeslu246@student.liu.se>
2017-03-30 15:54:06 +0200

    Added 3D effect (Side-by-side)

--- keyEffects.js
+++ keyEffects.js
@@ -6,6 +6,7 @@ function lightUp(key, startTime)
         if(noteBlockArray[i].note == key && noteBlockArray[i].startTime == startTime )
         {
             noteBlockArray[i].mesh.material.color.setHex(0x00ff00);
+            noteBlockArray[i].mesh.position.z = 2;
             break;
         }
     }  
@@ -19,6 +20,7 @@ function lightOff(key, stopTime)
         if(noteBlockArray[i].note == key && noteBlockArray[i].stopTime == stopTime)
         {
             noteBlockArray[i].mesh.material.color.setHex(0x0000ff);
+            noteBlockArray[i].mesh.position.z = 0;
             break;
         }
 
