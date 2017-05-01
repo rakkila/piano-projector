5342e968162f25b7cef601a98f5d23071c35c91d
Niklas Bergner <nikbe095@student.liu.se>
2017-05-01 20:47:45 +0200

    Bloom effects optimized.

--- keyEffects.js
+++ keyEffects.js
@@ -1,13 +1,14 @@
 function lightUp(key, startTime)
 { 
+
     for(let i = 0; i < noteBlockArray.length; ++i)
     {   
 
         if(noteBlockArray[i].note == key && noteBlockArray[i].startTime == startTime )
         {
             noteBlockArray[i].mesh.material.color.setHex(0x00ff00);
-                bloomIntensity = 10.0;
-                bloomSpread = 0.8;
+
+            //setBloomEffect(10);
          //   noteBlockArray[i].mesh.position.z = 2;
             break;
         }
@@ -25,6 +26,7 @@ function lightOff(key, stopTime)
 
             noteBlockArray[i].mesh.position.z = 0;
              line.material.color.setHex(0x050505);
+             //setBloomEffect(0);
 
             break;
         }
@@ -33,7 +35,17 @@ function lightOff(key, stopTime)
       
 }
 
-/*function fullScore()
+/*
+function setBloomEffect(b){
+    var bloom = b;
+}
+
+function getBloomEffect(){
+    console.log("bloom: " + bloom);
+    return bloom;
+}
+
+function fullScore()
 {
    fullScoreCounter = 0;
     noteBlockArray.mesh.material.color.setHex(0xffffff);
